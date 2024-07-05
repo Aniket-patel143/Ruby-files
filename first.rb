5825a4898b6a16c32fb6c7fffccaefa6b42138ef
Aniket-patel143 <parekhaniket3062@gmail.com>
2024-07-05 08:40:19 +0530

    Add files via upload

---
+++ first.rb
@@ -0,0 +1,32 @@
+a=30
+b=60
+c=40
+puts "value of a is=#{a}, b is #{b} & c is #{c}"
+
+puts "value of a+b=#{a+b}"
+
+
+
+# if a>b
+# 	puts "a is gt"
+
+# else
+# 	puts "b is gt"
+
+	
+#send
+
+# if a>b && a>c
+# 	puts "a is gt"
+
+
+# elsif b>c 
+# 	puts"b is grater"
+# else
+# 	puts"c is gt"
+			
+	
+# end
+
+
+
